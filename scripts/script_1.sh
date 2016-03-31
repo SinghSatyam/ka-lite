f96f31573708edf4e98852a0e15d89887d9963fc
Satyam Singh <satyam.mgs@gmail.com>
2016-03-31 17:36:47 +0530

    Initial commit :: Adding scripts

---
+++ scripts/script_1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root or use sudo" 
+   exit 1
+fi
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install vim vlc python-pip nginx git devscripts debhelper hostapd dnsmasq
+pip install ka-lite
---
+++ scripts/script_2.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root or use sudo" 
+   exit 1
+fi
+
+if [ "$#" -ne 1 ]
+then
+  echo "You need to pass the logged in username."
+  exit 1
+fi
+
+echo "Downloading the package lists from the repositories and updating them to get information on the newest versions of packages and their dependencies.."
+apt-get update
+echo "Package list updated.."
+echo "Installing python-pip and nginx"
+apt-get install python-pip nginx -y
+echo "Installation complete"
+echo "Removing default nginx configuration."
+rm -rf /etc/nginx/sites-enabled/default
+rm -rf /etc/nginx/sites-available/default
+echo "Removal done."
+echo "Configuring NGINX with $1 user..."
+echo 'upstream kalite {
+    server 127.0.0.1:9500;
+}
+
+server {
+
+    listen 8008;
+
+    location /static {
+        alias   /home/'$1'/.kalite/httpsrv/static/;
+    }
+
+    location /media {
+        alias   /home/'$1'/.kalite/media/;
+    }
+
+    location /content {
+        alias   /home/'$1'/.kalite/content/;
+    }
+
+    location /favicon.ico {
+        empty_gif;
+    }
+
+    location / {
+        proxy_set_header Host $http_host;
+        proxy_set_header X-Scheme $scheme;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_pass http://kalite;
+        error_page 502 = @502;
+    }
+
+    location @502 {
+        types { }
+        default_type "text/html";
+        return 502 "
+        <BR>
+        <H1>KA-Lite might be busy - wait a few moments and then reload this page
+        <BR><BR>
+        <H2>If KA-Lite is still busy, get help from the system administrator
+        <H3>Error code: nginx 502 Bad Gateway (maybe the KA-Lite webserver is not working correctly)";
+    }
+
+}' > /etc/nginx/sites-enabled/default
+echo 'upstream kalite {
+    server 127.0.0.1:9500;
+}
+
+server {
+
+    listen 8008;
+
+    location /static {
+        alias   /home/'$1'/.kalite/httpsrv/static/;
+    }
+
+    location /media {
+        alias   /home/'$1'/.kalite/media/;
+    }
+
+    location /content {
+        alias   /home/'$1'/.kalite/content/;
+    }
+
+    location /favicon.ico {
+        empty_gif;
+    }
+
+    location / {
+        proxy_set_header Host $http_host;
+        proxy_set_header X-Scheme $scheme;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_pass http://kalite;
+        error_page 502 = @502;
+    }
+
+    location @502 {
+        types { }
+        default_type "text/html";
+        return 502 "
+        <BR>
+        <H1>KA-Lite might be busy - wait a few moments and then reload this page
+        <BR><BR>
+        <H2>If KA-Lite is still busy, get help from the system administrator
+        <H3>Error code: nginx 502 Bad Gateway (maybe the KA-Lite webserver is not working correctly)";
+    }
+
+}' > /etc/nginx/sites-available/default
+echo "Nginx is configured with $1 user..."
+
+service nginx start
+
