840fb769c47af30016a1ffcc3a29176cdb72ea5a
Satyam Singh <satyam.mgs@gmail.com>
2016-06-02 18:36:12 +0530

    Getting deployment script

--- scripts/script_6.sh
+++ scripts/script_6.sh
@@ -2,15 +2,15 @@
 wget -q --spider http://github.com
 
 if [ $? -eq 0 ]; then
-	echo "You are online. Checking internet speed :"
+	echo "You are online!! Now checking internet speed :"
     	wget -O speedtest-cli -o /dev/null https://raw.github.com/sivel/speedtest-cli/master/speedtest_cli.py
 	chmod +x speedtest-cli
 	./speedtest-cli --simple
 	container_id=$(docker ps --filter="ancestor=singhsatyam/magogenie:v8" -q)
 	read -p "Are you sure you want CONTINUE at this speed(y/n)?" choice
 	case "$choice" in 
-	y|Y ) docker exec $container_id su - deployer -c "sh get_deployment_script.sh && docker exec $container_id su - deployer -c "sh update_mago.sh";;
-	n|N ) echo "no";;
+	y|Y ) docker exec $container_id su - deployer -c "sh wget http://www.magogenie.com/scripts/get_deployment_script.sh" && docker exec $container_id su - deployer -c "sh get_deployment_script.sh" && docker exec $container_id su - deployer -c "sh update_mago.sh";;
+	n|N ) echo "Get a life, get a good internet connection. :)";;
 	* ) echo "invalid";;
 	esac
 else
